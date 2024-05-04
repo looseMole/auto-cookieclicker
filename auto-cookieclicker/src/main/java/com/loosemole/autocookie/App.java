be22814c66c95f4496105baf6828dc18a1553205
looseMole <62012079+looseMole@users.noreply.github.com>
2024-05-04 20:34:56 +0200

    Initialize project

---
+++ auto-cookieclicker/src/main/java/com/loosemole/autocookie/App.java
@@ -0,0 +1,13 @@
+package com.loosemole.autocookie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
